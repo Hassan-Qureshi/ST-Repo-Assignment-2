f27ec75267d1fa6e0afaef87b732fb071c81c514
Ali Hassan Qureshi <alih.qureshi@hotmail.com>
2018-03-30 12:25:01 -0700

    Add files via upload

---
+++ src/simplejavacalculator/SimpleJavaCalculator.java
@@ -0,0 +1,16 @@
+/**
+ * @name        Simple Java Calculator
+ * @package     ph.calculator
+ * @file        Main.java
+ */
+
+package simplejavacalculator;
+
+public class SimpleJavaCalculator {
+
+    public static void main(String[] args) {
+        UI uiCal = new UI();
+        uiCal.init();
+    }
+
+}
